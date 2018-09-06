f58c69bb32788d99f8529cb1a2a3deb2af47169d
gogomogolo <gogolo1991@gmail.com>
2018-09-06 23:45:27 +0300

    constants change from other module

--- Simulation.py
+++ Simulation.py
@@ -1,6 +1,15 @@
+import parameters.Constants as Constants
+
 from generators.EndDeviceDistributor import distribute
 from distributions.Exponential import Exponential
 
 
+Constants.END_DEVICE_NUMBER = 100
+
 distribute(Exponential(6, 8))
 
+
+Constants.END_DEVICE_NUMBER = 10
+
+
+distribute(Exponential(6, 8))
---
+++ processors/Preprocessor.py
@@ -0,0 +1,7 @@
+import parameters.Constants as Constants
+
+
+def prepare_parameters(ed_n, mx_sf, mn_sf):
+    Constants.END_DEVICE_NUMBER = ed_n
+    Constants.MAX_SF = mx_sf
+    Constants.MIN_SF = mn_sf
