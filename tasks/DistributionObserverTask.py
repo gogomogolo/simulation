cbacae8c9aad04c8667fee60e3642ad4a22c10b1
Gökçer Yapar <gokcer.yapar@siemens.com>
2018-09-18 23:10:10 +0300

    refactoring

--- tasks/DistributionObserverTask.py
+++ tasks/DistributionObserverTask.py
@@ -12,7 +12,9 @@ class DistributionObserverTask(threading.Thread):
     def __init__(self, group_id, sf, end_devices, uplink_period_in_second):
         self.__group_id = group_id
         self.__sf = sf
-        self.__end_devices = end_devices
+        self.__end_devices = end_devices.copy()
+        self.__end_devices_success_transmission = []
+        self.__end_devices_fail_transmission = []
         self.__message_period_in_seconds = calculate_time_on_air(
             Constants.BANDWIDTH_IN_HERTZ,
             Constants.NUMBER_OF_PREAMBLE,
@@ -34,8 +36,9 @@ class DistributionObserverTask(threading.Thread):
         distribution = self.__create_distribution()
 
         while cycle < self.__time_slot_number:
-
+            self.__observe_transmission_of_end_devices(distribution)
             cycle += 1
+            self.__change_distribution(distribution)
             time.sleep(self.__message_period_in_seconds)
 
     def __create_distribution(self):
@@ -44,12 +47,25 @@ class DistributionObserverTask(threading.Thread):
 
     def __change_distribution(self, distribution):
         setattr(distribution, 'size', self.__observable_end_devices_count)
+        setattr(distribution, 'p', float(randrange(1, 10)) / float(self.__observable_end_devices_count))
 
     def __observe_transmission_of_end_devices(self, distribution):
-        self.__change_distribution(distribution)
         transmissions = distribution.sample()
         if self.__are_valid_transmissions(transmissions):
+            self.__update_end_device_container(transmissions, self.__end_devices_success_transmission)
+        else:
+            self.__update_end_device_container(transmissions, self.__end_devices_fail_transmission)
 
     def __are_valid_transmissions(self, transmissions):
-        return sum(transmissions) == 1
+        return sum(transmissions) <= 1
+
+    def __is_there_transmitter(self, transmissions):
+        return transmissions.count(1) != 0
+
+    def __update_end_device_container(self, transmissions, expected_updatable_device_container):
+        while self.__is_there_transmitter(transmissions):
+            end_device = self.__end_devices.pop(transmissions.index(1))
+            expected_updatable_device_container.append(end_device)
+            self.__observable_end_devices_count -= 1
+            transmissions.remove(1)
 
